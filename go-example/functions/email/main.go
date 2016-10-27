1793926372cd5602d0ad173e5da6c04e107b950b
mkuchin <mkuchin@gmail.com>
2016-10-27 18:51:34 +1100

    send email

--- go-example/functions/email/main.go
+++ go-example/functions/email/main.go
@@ -4,49 +4,107 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"log"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/ses"
 
 	"github.com/veqryn/go-email/email"
 )
 
+var connection *session.Session
+
 func main() {
 
-        bucket := "devspire-ses"
-        messageId := "pvnu7delckle60vnrmltf7nut04mrpisdqn3o0g1"
-        
-        body, err := getBody(bucket, messageId)
+	region := "us-east-1"
+	bucket := "devspire-ses"
+
+        var err error
+	connection, err = session.NewSession(&aws.Config{Region: aws.String(region)})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+
+//	messageId := "pvnu7delckle60vnrmltf7nut04mrpisdqn3o0g1"
+
+	text, err := getTextBody(bucket, messageId)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(text)
+        err = sendEmail(text, "test subj")
         if err != nil {
-                fmt.Println(err.Error())
-                return
-        }
+                log.Fatal(err)
+        } 
+
+}
+
+func sendEmail(text, subject string) error {
+        email := "xxxx@gmail.com"
+	svc := ses.New(connection)
+
+        // https://docs.aws.amazon.com/sdk-for-go/api/service/ses/#example_SES_SendEmail
+
+	params := &ses.SendEmailInput{
+		Destination: &ses.Destination{ // Required			
+			ToAddresses: []*string{
+				aws.String(email), // Required
+			},
+		},
+		Message: &ses.Message{ // Required
+			Body: &ses.Body{ // Required
+				Text: &ses.Content{
+					Data:    aws.String(text), // Required
+					Charset: aws.String("UTF-8"),
+				},
+			},
+			Subject: &ses.Content{ // Required
+				Data:    aws.String(subject), // Required
+			},
+		},
+		Source: aws.String(email), // Required
+	}
+	resp, err := svc.SendEmail(params)
+
+	if err != nil {
+		return err
+	}
+
+	// Pretty-print the response data.
+	fmt.Println(resp)
+        return nil
+}
+
+func getTextBody(bucket, key string) (string, error) {
+	body, err := getBody(bucket, key)
+	if err != nil {
+		return "", err
+	}
 
 	reader := bytes.NewReader(body)
 	msg, err := email.ParseMessage(reader)
 
 	if err != nil {
-		// Print the error, cast err to awserr.Error to get the Code and
-		// Message from an error.
-		fmt.Println(err.Error())
-		return
+		return "", err
 	}
 
+	var text string
 	for _, part := range msg.PartsContentTypePrefix("text/plain") {
-		fmt.Println(string(part.Body))
+		text = string(part.Body)
+		// fmt.Println(part.Header["Content-Type"])
+		// todo: parse "[text/plain; charset=UTF-8]"
 	}
+
+	return text, nil
 
 }
 
 func getBody(bucket, key string) ([]byte, error) {
-	region := "us-east-1"
-	sess, err := session.NewSession(&aws.Config{Region: aws.String(region)})
-	if err != nil {
-		return nil, err
-	}
-
-	svc := s3.New(sess)
+	svc := s3.New(connection)
 	params := &s3.GetObjectInput{
 		Bucket: aws.String(bucket), // required
 		Key:    aws.String(key),    // required
