a304ec8d19e252165dbc98740e15ac0abfb98dca
tcharding <me@tobin.cc>
2016-10-27 17:57:58 +1100

    Abstract body parsing into functions

--- go-example/functions/email/main.go
+++ go-example/functions/email/main.go
@@ -1,68 +1,56 @@
 package main
 
 import (
-    "fmt"
-    "bytes"
-    "io/ioutil"
-//    "net/mail"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	//    "net/mail"
 
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/s3"
-    
-    "github.com/veqryn/go-email/email"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+
+	"github.com/veqryn/go-email/email"
 )
 
 func main() {
-  region := "us-east-1"
-  sess, err := session.NewSession(&aws.Config{Region: aws.String(region)})
-  if err != nil {
-    fmt.Println("failed to create session,", err)
-    return
-  }
-  
-  svc := s3.New(sess)
-  params := &s3.GetObjectInput {
-    Bucket:                     aws.String("devspire-ses"), // Required
-    Key:                        aws.String("pvnu7delckle60vnrmltf7nut04mrpisdqn3o0g1"),  // Required
-  }
-  
 
-  resp, err := svc.GetObject(params)
+	//  fmt.Printf("%s", body)
 
-  if err != nil {
-    // Print the error, cast err to awserr.Error to get the Code and
-    // Message from an error.
-    fmt.Println(err.Error())
-    return
-   }
+	reader := bytes.NewReader(fullBody)
+	msg, err := email.ParseMessage(reader)
 
-  // Pretty-print the response data.
-  fmt.Println(resp)
-  
-//  b, err := ioutil.ReadAll(resp.Body)
-//  if err != nil {
-//     fmt.Println(err)
-//  }
-  
-  //m, err := mail.ReadMessage(resp.Body)
-  //body, err := ioutil.ReadAll(m.Body)
-  fullBody, err := ioutil.ReadAll(resp.Body)
-    
-//  fmt.Printf("%s", body)
+	if err != nil {
+		// Print the error, cast err to awserr.Error to get the Code and
+		// Message from an error.
+		fmt.Println(err.Error())
+		return
+	}
 
-  reader := bytes.NewReader(fullBody)
-  msg, err := email.ParseMessage(reader)
-  
-  if err != nil {
-    // Print the error, cast err to awserr.Error to get the Code and
-    // Message from an error.
-    fmt.Println(err.Error())
-    return
-   }
- 
-  for _, part := range msg.PartsContentTypePrefix("text/plain") {
-    fmt.Println(string(part.Body))
-  }
-  
+	for _, part := range msg.PartsContentTypePrefix("text/plain") {
+		fmt.Println(string(part.Body))
+	}
+
+}
+
+func getBody(bucket, key string) ([]byte, error) {
+	region := "us-east-1"
+	sess, err := session.NewSession(&aws.Config{Region: aws.String(region)})
+	if err != nil {
+		return nil, err
+	}
+
+	svc := s3.New(sess)
+	params := &s3.GetObjectInput{
+		Bucket: aws.String(bucket), // required
+		Key:    aws.String(key),    // required
+	}
+
+	resp, err := svc.GetObject(params)
+	if err != nil {
+		return nil, err
+	}
+
+	fullBody, err := ioutil.ReadAll(resp.Body)
+	return fullBody, err
 }
